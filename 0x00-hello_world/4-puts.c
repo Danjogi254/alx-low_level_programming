11819089b883c898594e0cd265b282a3236b0511
David Njoroge <dnjoroge1344@gmail.com>
2023-06-15 11:39:25 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
+/*
+ * main-A program that prints
+ * return : always 0 (success)
+ */
 {
-	puts ("/"Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
