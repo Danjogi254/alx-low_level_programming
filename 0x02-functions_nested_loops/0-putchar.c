7e7ae09f3565ccebae460c24746855de20212564
David Njoroge <dnjoroge1344@gmail.com>
2023-06-20 01:32:39 -0700

    Hello putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Description: 'This program prints _purchar'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar/n");
+	return (0);
+}
