5bf3b9adebf13d401ddd369ba997af889bfb4672
David Njoroge <dnjoroge1344@gmail.com>
2023-06-20 09:56:13 -0700

    Aphabets *ten

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 'Prints alphabets 10 times'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <=10; x++)
+	{
+		for (y = 97; y <=122; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
