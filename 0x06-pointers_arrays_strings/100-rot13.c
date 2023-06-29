1e7681588894253d4edbedbac951121f5634d4f3
David Njoroge <dnjoroge1344@gmail.com>
2023-06-29 10:14:54 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot 13
+ * @p: pointer
+ * Return: p
+ */
+char *rot13(char *p)
+{
+	int a;
+	int b;
+	char alp1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alp2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; p[a] != '\n'; a++)
+	{
+		for (b = 0; b < 52; b++)
+		{
+			if (p[a] == alp1[b])
+			{
+				p[a] = alp2[b];
+				break;
+			}
+		}
+	}
+	return (p);
+}
